88f63eda4a0f714fa882ae358787506b2218b593
Duality4y <201292@live.nl>
2013-03-26 01:33:57 +0100

    modified:   gtklearning/buildingfirsgui1.py 	new file:   gtklearning/buildingfirsgui2.py 	new file:   gtklearning/buildingfirstgui3.py

--- gtklearning/buildingfirsgui2.py
+++ gtklearning/buildingfirsgui2.py
@@ -8,66 +8,58 @@ class MyWindow(object):
 		window.connect("destroy", self.quit_gui)
 		window.set_title("My Window")
 		
+		#create the main Vertical Stack box that holds every other box.
 		mainVbox = gtk.VBox(False, 0)
 		window.add(mainVbox)
 		mainVbox.show()
 		
+		#create the main Menu box which will hold menu widgets and clicky buttons.
 		mainMenuBox = gtk.VBox(False, 0)
 		mainVbox.add(mainMenuBox)
 		mainMenuBox.show()
 		
+		#create the middle box which holds the tabs and text view. (basicly the text editor part)
 		textBox = gtk.VBox(False, 0)
 		mainVbox.add(textBox)
 		textBox.show()
 		
+		#create the bottom box which holds the terminal output, and progress bar.
 		loggerBox = gtk.VBox(False, 0)
-		mainVbox.add(loggerBox)
+		mainVbox.pack_end(loggerBox,False,False,0)
 		loggerBox.show()
 		
 		#split the main menu box in to two
 		upperMenuBox = gtk.VBox(False, 0)
 		lowerMenuBox = gtk.HBox(False, 0)
-		mainMenuBox.add(upperMenuBox)
-		mainMenuBox.add(lowerMenuBox)
+		mainMenuBox.pack_start(upperMenuBox, False, False, 0)
+		mainMenuBox.pack_start(lowerMenuBox, False, False, 0)
 		upperMenuBox.show()
 		lowerMenuBox.show()
-		
 		
 		#add buttons to the lowerMenuBox
 		for i in range(6):
 			if i == 5:
-				button = gtk.Button("button 6")
+				button = gtk.Button("6")
 				lowerMenuBox.pack_end(button, False, False, 0)
 			else:
-				button = gtk.Button("button %i" % (i))
+				button = gtk.Button("%i" % (i))
 				lowerMenuBox.pack_start(button, False, False, 0)
-				button.show()
+			button.set_size_request(30,30)
+			button.show()
 		
-		#create a menu add it to the upperMenu box
-		#create the menu bar that holds every thing
-		menubar = gtk.MenuBar()
-		#create a menu in the menu bar
-		menu_file = gtk.Menu()
-		#create a item that wil bee hold by the menu
-		item_open = gtk.MenuItem("Open")
-		#add the item to the menu
-		menu_file.append(item_open)
-		#create and item for the menu.
-		item_file = gtk.MenuItem("File")
-		#add the item to the menubar
-		item_file.set_submenu(menu_file)
-		#add the actual menu, to the menu bar
-		menubar.append(item_file)
-		
-		upperMenuBox.add(menubar)
+		thisDict = {"File":["New","Open","Save","Close","Quit"],
+					"Edit":["Undo","Redo","Cut","Copy","Paste"],}
+		#upperMenuBox.add(self.create_menu(thisDict))
 		
 		window.show_all()
 	def create_menu(self, box, menuItems):
+		print "dict: >> %s" % menuItems
 		menubar = gtk.MenuBar()
 		for MenuItem in menuItems:
 			for subMenuItem in menuItems[MenuItem]:
 				print "MenuItem: %s | subMenuItem: %s" % (MenuItem, subMenuItem)
-		print "dict: >> %s" % menuItems
+		return menubar
+		
 	def quit_gui(self, widget):
 		gtk.main_quit()
 	def run_gui(self):
